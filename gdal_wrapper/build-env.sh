7fcaeabc30789ccd0355b92cc4729e541ec42e67
grallewellyn <grace.llewellyn@jpl.nasa.gov>
2024-06-07 10:47:53 -0700

    added testing files with dummy code

---
+++ gdal_wrapper/build-env.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash --login
+set -euo pipefail
+# This script is used to install any custom packages required by the algorithm.
+
+# Get current location of build script
+basedir=$( cd "$(dirname "$0")" ; pwd -P )
---
+++ gdal_wrapper/run_gdal.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env -S bash --login
+set -euo pipefail
+# This script is the one that is called by the DPS.
+# Use this script to prepare input paths for any files
+# that are downloaded by the DPS and outputs that are
+# required to be persisted
+
+# Get current location of build script
+basedir=$(dirname "$(readlink -f "$0")")
+
+# Create output directory to store outputs.
+# The name is output as required by the DPS.
+# Note how we dont provide an absolute path
+# but instead a relative one as the DPS creates
+# a temp working directory for our code.
+
+mkdir -p output
